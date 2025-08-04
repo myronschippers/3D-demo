f92d9fb36398292ac2a8242aa9d1ccae20e979af
Myron R Schippers Jr <dubtwee@gmail.com>
2025-08-04 13:26:59 -0400

    getting a spinning cube to show up in the window

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import * as THREE from 'three';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
 import './style.css';
 // import javascriptLogo from './javascript.svg';
@@ -8,12 +9,25 @@ const scene = new THREE.Scene();
 
 const aspectRatio = window.innerWidth / window.innerHeight;
 const camera = new THREE.PerspectiveCamera(75, aspectRatio, 0.1, 1000);
-const renderer = new THREE.WebGL3DRenderTarget({
-  canvas: document.querySelector('#demo'),
-});
 
-renderer.setPixelRatio(window.devicePixelRatio);
+const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
-camera.position.setZ(30);
+document.querySelector('#demo').appendChild(renderer.domElement);
 
-renderer.render(scene, camera);
+// later used for controlling the cube player
+const controls = new OrbitControls(camera, renderer.domElement);
+
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+
+camera.position.z = 5;
+
+function animate() {
+  requestAnimationFrame(animate);
+  renderer.render(scene, camera);
+  cube.rotation.x += 0.01;
+  cube.rotation.y += 0.01;
+}
+animate();
